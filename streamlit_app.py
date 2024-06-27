c0b2124ecc91cae4fd0473827fa8b2465eb9e7ae
alphasec <95212748+alphasecio@users.noreply.github.com>
2024-06-27 21:52:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ with st.sidebar:
   option = st.radio("Use Gemini to...", [
     "Answer a question",
     "Caption an image",
-    "Blog about an image"
+    "Blog about an image",
+    "Summarize a transcript"
   ])
 
 if option == "Answer a question":
@@ -56,6 +57,32 @@ elif option == "Blog about an image":
           genai.configure(api_key=google_api_key)
           model = genai.GenerativeModel("gemini-pro-vision")
           response = model.generate_content([prompt, PIL.Image.open(image)], stream=False)
+          st.success(response.text)
+      except Exception as e:
+        st.exception(f"Exception: {e}") 
+elif option == "Summarize a transcript":
+  prompt = """Please read through the following transcript carefully. Then, write a concise summary of the key topics, ideas and conclusions covered in the discussion between the host and the guest(s). 
+  
+  Organize your summary into clear sections with descriptive headers, and use bullet points to list the main takeaways under each section.
+  
+  Some tips:
+  - Focus on capturing the essence and key points succinctly, in your own words. Do not include any long verbatim quotes from the transcript.
+  - Ignore any preamble and information about the guest's background, experience etc, and focus only on the key topic being discussed.
+  - Aim for an overall summary length of around 400-600 words. 
+  - Use a structured format to make the summary easy to read and navigate.
+
+  Here is the full transcript of the podcast episode to summarize:
+  """
+  transcript = st.text_area("Podcast Transcript", label_visibility="collapsed", height=200)
+  if st.button("Submit"):
+    if not google_api_key.strip() or not transcript.strip():
+      st.error("Please provide the missing fields.")
+    else:
+      try:
+        with st.spinner("Please wait..."):
+          genai.configure(api_key=google_api_key)
+          model = genai.GenerativeModel("gemini-pro")
+          response = model.generate_content(prompt, stream=False)
           st.success(response.text)
       except Exception as e:
         st.exception(f"Exception: {e}") 
